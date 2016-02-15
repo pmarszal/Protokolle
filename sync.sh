f0bada6e1899c3974f40d6c0e21332085260e705
pmarszal <philip.marszal@stud.uni-goettingen.de>
2016-02-15 19:11:56 +0100

    rbk temperaturplots und durchführung hinzugefügt

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#git clone https://github.com/pmarszal/F-Praktikum.git
+
+git remote add upstream https://github.com/vtelezki/F-Praktikum.git 
+
+git fetch upstream
+
+#git checkout master
+
+#git merge upstream/master
